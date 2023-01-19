aaa86d30c2d11f03cf5e9fef6d94351de13891b5
Jarod Smith <lbriones.dev@gmail.com>
2023-01-19 19:21:19 -0300

    pedidos/clientes : Validación datos/Maquetación de formulario

--- js/pedidos.js
+++ js/pedidos.js
@@ -12,7 +12,47 @@ $(document).ready(function(){
         )
     })
 
+// validación para el Botón Guardar el form
+
+// Campos del formulario
+cliente = $("#cliente");
+f_compra = $("#fecha");
+tel = $("#telefono");
+
+productos = $("#productos");
+$("#send_form").on("click", function(){
+
+    if(cliente.val() == ""){
+        lanzarSweetAlertError("Nombre Cliente");
+        return false
+        }
+
+    if (f_compra.val() == ""){ 
+        lanzarSweetAlertError("Fecha de Compra");
+        return false
+        }
+
+    if (tel.val() == ""){
+        lanzarSweetAlertError("Teléfono");
+        return false
+        }
+
+        Swal.fire(
+            {title: "¡Perfecto!.",
+            text: "Pedido Almacenado Correctamente.",
+            icon: 'success'}
+        )
+    })
+
+    
+
 })
 
-
-
+function lanzarSweetAlertError(campo){
+    let mensaje_text = `${campo} no puede estar vacío, favor complételo.`
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops, algo salió mal',
+        text: mensaje_text,
+      })
+}
